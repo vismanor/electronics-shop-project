23b3673a2e2a71b056ff669f022d0b3d6f83af72
Katya Smirnova <vismanork@gmail.com>
2024-02-18 04:08:28 +0300

    homework-2 was successfully finished

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+import os
+from math import floor
+
+
 class Item:
     """
         Создаем класс для представления товара в магазине
@@ -5,17 +10,28 @@ class Item:
     pay_rate = 1.0  # Хранит уровень цен с учетом скидки (н: 0.85, при скидке 15%)
     all = []  # Хранит созданные экземпляры класса
 
-    def __init__(self, product_name: str, price: float, amount: int) -> None:
+    def __init__(self, name: str, price: float, amount: int) -> None:
         """
             Добавляем атрибуты:
-            :param product_name: название товара
+            :param name: название товара
             :param price: цена за единицу товара
             :param amount: количество товара в магазине
         """
-        self.product_name = product_name
+        self.__name = name  # Сделали атрибут name приватным
         self.price = price
         self.amount = amount
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            self.__name = value[:10]  # Проверяем, что наименование товара не больше 10 симвовов, обрезаем, если больше
+        else:
+            self.__name = value
 
     def calculate_total_price(self) -> float:
         """
@@ -25,7 +41,33 @@ class Item:
 
     def apply_discount(self) -> float:
         """
-            Метод принемяет установленную скидку для конкретного товара
+            Метод принимает установленную скидку для конкретного товара
         """
         self.price *= self.pay_rate
         return self.price
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path='src/items.csv'):
+        """
+            Класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_
+        """
+        Item.all.clear()
+        script_dir = os.path.dirname(__file__)  # Получаем путь к текущему файлу item.py
+        file_path = os.path.join(script_dir, '..', file_path)  # Объединяем путь к файлу items.csv
+        with open(file_path, 'r', newline='', encoding='UTF-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                amount = int(row['quantity'])
+                cls(name, price, amount)
+
+    @staticmethod
+    def string_to_number(num_str: str) -> int:
+        """
+            Статический метод, возвращающий число из числа-строки
+        """
+        try:
+            return int(float(num_str))
+        except ValueError:
+            raise ValueError('Ай-яй! Некорректный ввод. Введи допустимое числовое значение :)')
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,5 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
 from src.item import Item
 
 
@@ -18,3 +19,45 @@ def test_apply_discount():
 
     assert item1.apply_discount() == 8000
     assert item2.apply_discount() == 16000
+
+
+def test_name_getter():
+    item = Item('Телефон', 189990, 2)
+    assert item.name == 'Телефон'
+
+
+def test_name_setter():
+    item = Item('Телефон', 189990, 2)
+    assert item.name == 'Телефон'
+
+    item = Item('МегаУльтраЭкстраТелефон', 89990, 1)
+    item.name = 'МегаУльтраЭкстраТелефон'
+    assert item.name == 'МегаУльтра'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+
+    assert len(Item.all) == 5
+
+    item1 = Item.all[3]
+    item2 = Item.all[1]
+
+    assert item1.name == 'Мышка'
+    assert item2.name == 'Ноутбук'
+
+    item3 = Item.all[4]
+
+    assert item3.name == 'Клавиатура'
+    assert item3.price == 75
+    assert item3.amount == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('-2.3') == -2
+    assert Item.string_to_number('-3') == -3
+    assert Item.string_to_number('0') == 0
+    assert Item.string_to_number('0.0') == 0
+    assert Item.string_to_number('4') == 4
+    with pytest.raises(ValueError):
+        Item.string_to_number('abc')
